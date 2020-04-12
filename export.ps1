00e02e631038769678797ab19241b0ba8f129187
Vincent Rémond <vincent.remond@gmail.com>
2020-04-12 17:04:26 +0200

    Add tombola

--- export.ps1
+++ export.ps1
@@ -1,26 +1,22 @@
-function ExportImage ($name, $width, $height) {
-    Write-Host """$($name).png"""
-    Start-Process "C:\Program Files\Inkscape\inkscape.exe" -Wait -ArgumentList ( `
-        "-f", """$($name).svg""", `
-        "-e", """$($name).png"""
-        )
-    for ($x = 0; $x -lt $width; $x++) {
-        for ($y = 0; $y -lt $height; $y++) {
-            $x0 = ($x + 0) * 32
-            $x1 = ($x + 1) * 32
-            $y0 = ($y + 0) * 32
-            $y1 = ($y + 1) * 32
-            $ef = "$($name)-$($height - $y)-$($x + 1).png"
-            Write-Host "$ef"
-            Start-Process "C:\Program Files\Inkscape\inkscape.exe" -Wait -ArgumentList ( `
-                "-f", """$($name).svg""", `
-                "-e", """$($ef)""", `
-                "--export-area=""$($x0):$($y0):$($x1):$($y1)""" `
-                )
-        }
+param ($name, $width, $height)
+
+Write-Host """$($name).png"""
+Start-Process "C:\Program Files\Inkscape\inkscape.exe" -Wait -ArgumentList ( `
+    "-f", """$($name).svg""", `
+    "-e", """$($name).png"""
+    )
+for ($x = 0; $x -lt $width; $x++) {
+    for ($y = 0; $y -lt $height; $y++) {
+        $x0 = ($x + 0) * 32
+        $x1 = ($x + 1) * 32
+        $y0 = ($y + 0) * 32
+        $y1 = ($y + 1) * 32
+        $ef = "$($name)-$($height - $y)-$($x + 1).png"
+        Write-Host "$ef"
+        Start-Process "C:\Program Files\Inkscape\inkscape.exe" -Wait -ArgumentList ( `
+            "-f", """$($name).svg""", `
+            "-e", """$($ef)""", `
+            "--export-area=""$($x0):$($y0):$($x1):$($y1)""" `
+            )
     }
 }
-
-ExportImage "forbidden" 2 1
-ExportImage "test" 2 2
-ExportImage "gift" 2 1
